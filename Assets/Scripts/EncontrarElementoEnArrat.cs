936450405a68518b30b4c19d5484f47e46420026
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-11-17 14:29:53 -0300

    Initial commit

---
+++ Assets/Scripts/EncontrarElementoEnArrat.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncontrarElementoEnArrat : MonoBehaviour
+{
+    public GameObject[] array_de_mesas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        array_de_mesas = GameObject.FindGameObjectsWithTag("Mesa");
+        addMesaScriptAndSetDestructible();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            //desactivarMesas();
+            destroyDestructible();
+        }
+    }
+
+    void desactivarMesas()
+    {
+        for(int i = 0; i < array_de_mesas.Length; i++)
+        {
+            array_de_mesas[i].SetActive(false);
+        }
+    }
+
+    void addMesaScriptAndSetDestructible()
+    {
+        foreach(GameObject mesa in array_de_mesas)
+        {
+            mesa.AddComponent<Mesa>();
+            mesa.GetComponent<Mesa>().destructible = Random.Range(0, 2) == 0;
+        }
+    }
+
+    void destroyDestructible()
+    {
+        foreach (GameObject mesa in array_de_mesas)
+        {
+            if (mesa.GetComponent<Mesa>().destructible)
+            {
+                Destroy(mesa);
+            }
+        }
+    }
+}
